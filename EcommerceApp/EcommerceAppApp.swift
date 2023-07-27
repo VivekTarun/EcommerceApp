92772bc79822bf5199de9d19db8695cbaf64fbed
vivektarun <vivektarun1234@gmail.com>
2023-07-27 19:00:17 +0530

    initial file added

---
+++ EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by Vivek Tarun on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
